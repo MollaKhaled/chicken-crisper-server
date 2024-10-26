620650b061bfa2d6c8c26a539c3946e88a641783
Mollakhaled <khaledbalok@gmail.com>
2024-10-26 22:40:28 +0600

    remove console

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { MongoClient, ServerApiVersion, ObjectId, Admin } = require('mongodb');
 app.use(cors())
 app.use(express.json());
 
-console.log(process.env.DB_PASS);
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dhrn8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
